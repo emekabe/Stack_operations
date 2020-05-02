1a2ddbcd13d07ee8a0cedfb787f6f0158a6d3cc8
Emeka Chukwulobe <emekabe@yahoo.com>
2020-05-02 12:00:24 +0100

    Common stack operations

--- src/stackoperations/StackOperations.java
+++ src/stackoperations/StackOperations.java
@@ -4,8 +4,31 @@ package stackoperations;
  * @author Emeka
  */
 
+import java.util.Stack;
+
 public class StackOperations {
     public static void main(String[] args) {
+        Stack <Integer> myStack = new Stack();
         
+        /*  Common Stack operations  */
+        //push(Integer)
+        myStack.push(5);
+        myStack.push(3);
+        myStack.push(4);
+        myStack.push(7);
+        
+        // [5, 3, 4, 7]
+        System.out.println(myStack);
+        
+        //pop()
+        myStack.pop();
+        
+        //[5, 3, 4]
+        System.out.println(myStack);
+        
+        //peek
+        
+        // 4
+        System.out.println(myStack.peek());
     }
 }
